9777b246f03942cc6d34a9032eb1b49a082e6a30
marui <markmarui@vip.qq.com>
2020-04-17 01:22:13 +0800

    Initial commit

---
+++ admin/src/test/java/com/ray/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
